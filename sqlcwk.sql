58dde4d3f290ab1be3b983c61ce5fac777e1707f
pith <ollyc88@gmail.com>
2023-03-10 19:04:06 +0000

    completed question 3

--- sqlcwk.sql
+++ sqlcwk.sql
@@ -28,16 +28,8 @@ WARNING: DO NOT REMOVE THE STATEMENT "CREATE VIEW vCustomerPerEmployee AS"
 */
 
 CREATE VIEW vCustomerPerEmployee  AS
---Remove this line and complete your query for question 1 here
-
---SELECT "LastName" AS LastName, "FirstName" AS FirstName, "EmployeeID" AS EmployeeID FROM employees
-
---SELECT SupportRepId, COUNT(SupportRepId) As TotalCustomers FROM customers
---GROUP BY SupportRepId;
-
 
 SELECT e.LastName, e.FirstName, e.EmployeeID, COUNT(c.SupportRepId) AS TotalCustomers
-
 FROM employees e
 LEFT JOIN customers c
 on e.EmployeeID = c.SupportRepiD
@@ -46,26 +38,70 @@ GROUP BY EmployeeID;
 /*
 ============================================================================
 Question 2: Complete the query for v10WorstSellingGenres.
-WARNNIG: DO NOT REMOVE THE STATEMENT "CREATE VIEW v10WorstSellingGenres AS"
+WARNING: DO NOT REMOVE THE STATEMENT "CREATE VIEW v10WorstSellingGenres AS"
 ============================================================================
 */
 
---CREATE VIEW v10WorstSellingGenres  AS
---Remove this line and complete your query for question 2 here
+CREATE VIEW v10WorstSellingGenres  AS
 
+SELECT g.Name AS Genre, COUNT(c.CustomerId) AS Sales
 
+FROM genres g
+JOIN tracks t
+ON g.GenreId = t.GenreId
+
+LEFT JOIN invoice_items ii
+ON ii.TrackId = t.TrackId
+
+LEFT JOIN invoices i
+ON i.InvoiceId = ii.InvoiceId
+
+LEFT JOIN customers c
+ON c.CustomerId = i.CustomerId
+
+GROUP BY g.Name 
+ORDER BY Sales ASC;
 
 /*
 ============================================================================
 Question 3:
 Complete the query for vBestSellingGenreAlbum
-WARNNIG: DO NOT REMOVE THE STATEMENT "CREATE VIEW vBestSellingGenreAlbum AS"
+WARNING: DO NOT REMOVE THE STATEMENT "CREATE VIEW vBestSellingGenreAlbum AS"
 ============================================================================
 */
 
---CREATE VIEW vBestSellingGenreAlbum  AS
---Remove this line and complete your query for question 3 here
+CREATE VIEW vBestSellingGenreAlbum  AS
+SELECT Genre, Album, Artist, Sales
+FROM(
+    SELECT g.Name AS Genre,
+    al.Title AS Album,
+    ar.Name AS Artist,
+    COUNT(c.CustomerId) AS Sales,
+    ROW_NUMBER() OVER (PARTITION BY g.Name ORDER BY COUNT(c.CustomerId) DESC) AS Rank
 
+    FROM genres g
+    JOIN tracks t
+    ON g.GenreId = t.GenreId
+
+    JOIN invoice_items ii
+    ON ii.TrackId = t.TrackId
+
+    JOIN invoices i
+    ON i.InvoiceId = ii.InvoiceId
+
+    JOIN customers c
+    ON c.CustomerId = i.CustomerId
+
+    JOIN albums al
+    on al.AlbumId = t.AlbumId
+
+    JOIN artists ar
+    on al.ArtistId = ar.ArtistId
+
+    GROUP BY Album
+    --ORDER BY Genre, Sales DESC
+)
+WHERE Rank = 1;
 
 
 /*
@@ -75,9 +111,7 @@ Complete the query for v10BestSellingArtists
 WARNNIG: DO NOT REMOVE THE STATEMENT "CREATE VIEW v10BestSellingArtists AS"
 ============================================================================
 */
-
---CREATE VIEW v10BestSellingArtists AS
---Remove this line and complete your query for question 4 here
+-- CREATE VIEW v10BestSellingArtists AS
 
 
 
@@ -89,15 +123,14 @@ WARNNIG: DO NOT REMOVE THE STATEMENT "CREATE VIEW vTopCustomerEachGenre AS"
 ============================================================================
 */
 --CREATE VIEW vTopCustomerEachGenre AS
---Remove this line and complete your query for question 5 here
 
 
 /*
 To view the created views, use SELECT * FROM views;
 You can uncomment the following to look at invididual views created
 */
-SELECT * FROM vCustomerPerEmployee;
+--SELECT * FROM vCustomerPerEmployee;
 --SELECT * FROM v10WorstSellingGenres;
---SELECT * FROM vBestSellingGenreAlbum;
+SELECT * FROM vBestSellingGenreAlbum;
 --SELECT * FROM v10BestSellingArtists;
 --SELECT * FROM vTopCustomerEachGenre;
